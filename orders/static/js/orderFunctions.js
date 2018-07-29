83e84ab79026001e95c9448dc9208b60b0164175
unknown <radler@imprivata.com>
2018-07-29 19:06:34 -0400

    added in local storage shopping cart

--- orders/static/js/orderFunctions.js
+++ orders/static/js/orderFunctions.js
@@ -14,27 +14,39 @@ function renderPizzaItemTitle(item, itemToAppend) {
 function renderSizeForm(item, itemToAppend) {
 
   console.log("render size form")
-  var formDiv = document.createElement('div');
+  var formDiv = document.createElement('form');
   formDiv.id = 'orderForm'
-  // renderlargeSize Option
+
+  //create Subtitle
 
   formDiv.appendChild(buildSizeForm(item.largePrice, "Large"));
   formDiv.appendChild(buildSizeForm(item.smallPrice, "Small"))
 
   //attach to form
-  itemToAppend.appendChild(formDiv)
+  var subTitle = document.createElement('h4');
+  var hr = document.createElement('hr');
+  subTitle.appendChild(document.createTextNode("Select a Size"));
+  itemToAppend.appendChild(subTitle);
+  itemToAppend.appendChild(hr);
+  itemToAppend.appendChild(formDiv);
 }
 
 function renderToppingsForm(item) {
   var itemSection = document.getElementById('itemContainerSection');
-  console.log(item.length);
-  for (i = 0; i < item.length; i++) {
-    console.log(item.length)
-    var option = renderToppingsOption(item[i].fields.name)
-    console.log(item[i]);
-    itemSection.appendChild(option);
-  }
+  var subTitle = document.createElement('h4');
+  var hr = document.createElement('hr');
+  subTitle.appendChild(document.createTextNode("Select Toppings"));
+  itemSection.appendChild(subTitle);
+  itemSection.appendChild(hr)
+  var div = document.createElement('form');
+  div.setAttribute('class', 'form-check');
 
+//loop through all toppings delived to page and render them
+  for (i = 0; i < item.length; i++) {
+    div = renderToppingsOption(item[i].fields.name, div)
+  }
+ itemSection.appendChild(div);
+ itemSection.appendChild(hr);
 }
 
 function buildSizeForm(cost, nameOfOption) {
@@ -43,7 +55,8 @@ function buildSizeForm(cost, nameOfOption) {
   var input = document.createElement('input');
   input.setAttribute('type', 'radio');
   input.setAttribute('name', nameOfOption);
-  input.setAttribute('value', cost);
+  input.setAttribute('value',cost);
+  input.setAttribute('class',"sizeForm");
   var label = document.createElement('label');
   label.setAttribute('class', 'form-check-label');
   label.setAttribute('for', nameOfOption);
@@ -55,9 +68,9 @@ function buildSizeForm(cost, nameOfOption) {
   return div
 }
 
-function renderToppingsOption(nameOfOption) {
-  var div = document.createElement('div');
-  div.setAttribute('class', 'form-check');
+function renderToppingsOption(nameOfOption,div) {
+  // var div = document.createElement('div');
+  // div.setAttribute('class', 'form-check');
 
   var input = document.createElement('input');
   input.setAttribute('type', 'checkbox');
@@ -77,11 +90,80 @@ function renderToppingsOption(nameOfOption) {
 
 }
 
-function addAddToOrderButtion() {
+function addAddToOrderButton() {
+var itemSection = document.getElementById('itemContainerSection');
+
+//create button
+var button = document.createElement('button');
+button.setAttribute('type','submit');
+button.setAttribute('class',"btn btn-primary");
+button.appendChild(document.createTextNode("Add to Order"));
+button.setAttribute('onclick',"addToOrder()")
+
+itemSection.appendChild(button);
+}
+
+function toppingsSelectionValidator() {
 
 }
 
+function getNumberOfToppingsSelected() {
+  var selections = document.getElementsByClassName("form-check-input")
+  var count = 0;
+  for (var i = 0; i < selections.length; i++) {
+    if (selections[i].type === "checkbox" && selections[i].checked === true) {
+        count++;
+    }
+  }
+  return count;
+}
 
+function addToOrder(){
+  var itemProperties = itemData[0].fields;
+  var size = getSize();
+  var toppings = getToppings();
+  var orderItem = {'size' : size.name,
+                   'cost': size.cost,
+                   'category': itemProperties.category,
+                   'name': itemProperties.name,
+                   'toppings': toppings
+                  }
+console.log(orderItem);
+updateShoppingCart(orderItem);
+
+
+}
+function getSize() {
+  var size = document.getElementsByClassName('sizeForm');
+  for(i=0; i < size.length; i++) {
+    if(size[i].checked) {
+      var size =  {'cost': size[i].value,
+                     'name': size[i].name
+                   };
+      return size;
+    }
+  }
+}
+
+function getToppings() {
+  var toppingsList = [];
+  var toppingSelection = document.getElementsByClassName('form-check-input');
+  for (i=0; i < toppingSelection.length; i++) {
+    if(toppingSelection[i].checked) {
+      toppingsList.push(toppingSelection[i].name);
+    }
+  }
+    return toppingsList;
+}
+
+function updateShoppingCart(orderItem) {
+  var shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+  if (!shoppingCart) {
+    shoppingCart = [];
+  }
+  shoppingCart.push(orderItem);
+  localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart))
+}
 
 function toCamelCase(str) {
   str = str.split(" ");
